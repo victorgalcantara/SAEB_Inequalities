823574de1b9fa394c699cc4760311961a291042d
Victor Gabriel Alcantara <67282866+victorgalcantara@users.noreply.github.com>
2022-01-04 23:09:36 -0300

    Add files via upload

--- LinearModel_Multilevel/lm_students.R
+++ LinearModel_Multilevel/lm_students.R
@@ -6,20 +6,22 @@
 
 library(tidyverse)
 library(readxl)
+library(corrplot)
 
 # 1. Openning data ===========================================================
 # Diretório com as bases de dados que iremos trabalhar guardado no objeto 'wd'
-wd <- "C:/Users/VictorGabriel/documents/00_dados/EDUCACAO/"
-wd <- "E:/VGA/dados/"
+wd <- "C:/Users/VictorGabriel/Documents/00_dados/"
+wd <- "E:/VGA/dados/EDUCACAO/"
 setwd(wd)
 
-ano = seq(from=2013,to=2019,by=2) # anos em que Saeb passou a ser censitário
+ano = seq(from=2011,to=2019,by=2) # anos em que Saeb passou a ser censitário
 
-saeb <- list(s13 = data.frame(), s15 = data.frame(), s17 = data.frame(),
+saeb <- list(s11=data.frame(),s13 = data.frame(),
+             s15 = data.frame(), s17 = data.frame(),
              s19 = data.frame())
 
-for(i in c(1:3)) { # 2013-2017
-  saeb[[i]] <- read_csv(paste0(wd,"EDUCACAO/SAEB/",ano[i],"/DADOS/TS_ALUNO_9EF.csv")) %>%  
+for(i in c(2)) { # 2013-2017
+  saeb[[i]] <- read_csv(paste0(wd,"SAEB/",ano[i],"/DADOS/TS_ALUNO_9EF.csv")) %>%  
     select(
            ID_REGIAO, ID_UF, ID_MUNICIPIO,ID_LOCALIZACAO,
            ID_ALUNO,ID_ESCOLA, ID_TURMA,
@@ -67,8 +69,11 @@ for(i in c(4)) { # 2019
 }
 
 # Indicadores construidos
-load("capitalCultural.RDS")
-load("capitalEconomico.RDS")
+load("../capitalCultural.RDS")
+load("../capitalEconomico.RDS")
+load("estruturaEscola13-17.RDS")
+idhm <- read_xlsx(paste0(wd,"ATLAS DESIG/ATLAS_2013.xlsx"),sheet = 2)
+idhm <- idhm %>% select(IDHM,Codmun7)
 
 # Regioes
 regioes <- read_excel(paste0(wd,"RELATORIO_DTB_BRASIL_MUNICIPIO.xls")) %>% 
@@ -87,11 +92,12 @@ regioes <- read_excel(paste0(wd,"RELATORIO_DTB_BRASIL_MUNICIPIO.xls")) %>%
 
 # 2. Data management ----------------------------------------------------------
 
-my_saeb <- list(s13 = data.frame(), s15 = data.frame(),
+my_saeb <- list(s11 = data.frame(),s13 = data.frame(),
+                s15 = data.frame(),
                 s17 = data.frame(),s19 = data.frame())
 
-for(i in 1:3) {
-  my_saeb[[i]] <- saeb[[i]] %>%  mutate(., 
+for(i in 2) {
+  saeb[[i]] <- saeb[[i]] %>%  mutate(., 
                                            
                                            regiao = case_when(
                                              
@@ -167,10 +173,39 @@ for(i in 1:3) {
 ) }
 
 # Imputando capitais na base dos alunos
-for( i in 3 ) { for( j in 4 ) {
-  my_saeb[[i]] <- merge (my_saeb[[i]],capitalCultural[[j]],by = "ID_ALUNO")
-  my_saeb[[i]] <- merge (my_saeb[[i]],capitalEconomico[[j]],by = "ID_ALUNO")
-}}
+for( i in 2 ) {
+  my_saeb[[i]] <- merge (my_saeb[[i]],capitalCultural[[i]],by = "ID_ALUNO")
+  my_saeb[[i]] <- merge (my_saeb[[i]],capitalEconomico[[i]],by = "ID_ALUNO")
+  my_saeb[[i]] <- merge (my_saeb[[i]],estruturaEscola[[i]],by = "ID_ESCOLA",all.x = T)
+}
+save(my_saeb,file="mysaeb13.RDS")
+
+load("../samples_n10.RDS")
+
+my_saeb <- my_samples[[3]] %>% select(
+  ID_ALUNO,ID_ESCOLA,ID_REGIAO,ID_MUNICIPIO,ID_UF,
+  ID_LOCALIZACAO,PROFICIENCIA_LP_SAEB,PROFICIENCIA_MT_SAEB,
+  IN_PREENCHIMENTO_QUESTIONARIO,IN_PREENCHIMENTO_PROVA,
+  regiao,fluxo,sexo,raca,raca2,trabFora,trabDom,educPai,educMae,
+  CC,CE
+)
+
+data <- merge(my_saeb,estruturaEscola[[3]],by = "ID_ESCOLA")
+mydata <- merge(data,idhm,by.x="ID_MUNICIPIO.x",by.y = "Codmun7")
+
+mydata <- mydata %>% rename(.,
+                            "Proficiência \n aluno \n LP" = aluno_LP,
+                            "Proficiência \n aluno \n MT" = aluno_MT,
+                            "Proficiência \n média escola \n LP"   = esc_LP,
+                            "Proficiência \n média escola \n MT"   = esc_MT,
+                            "Percentual \n docentes \n com ES"     = prof,
+                            "Indicador \n estrutura \n escolar" = estr_esc,
+                            "Indicador \n Capital \n Cultural" = CC,
+                            "Indicador \n Capital \n Econômico" = CE,
+                            "Escolaridade \n Pai"  = educPai,
+                            "Escolaridade \n Mãe"  = educMae,
+                            
+                            )
 
 # Somente alunos que preencheram a prova
 for(i in 1:3) {
@@ -183,10 +218,26 @@ for(i in 1:3) {
 my_matrix <- list(s13 = data.frame(), s15 = data.frame(),
                   s17 = data.frame(),s19 = data.frame())
 
-for(i in 1:3) {
-my_matrix[[i]] <- my_saeb[[i]] %>% select(PROFICIENCIA_LP_SAEB,PROFICIENCIA_MT_SAEB,
-                                          CC2, CE2,educMae,educPai) %>% na.exclude() %>% as.matrix()}
-t<-cor(my_matrix[[1]])
+for(i in 3) {
+my_matrix[[i]] <- mydata %>% select(
+  "Proficiência \n aluno \n LP","Proficiência \n aluno \n MT",
+  "Proficiência \n média escola \n LP",
+  "Proficiência \n média escola \n MT",
+  "Percentual \n docentes \n com ES",
+  "Indicador \n estrutura \n escolar",
+  "Indicador \n Capital \n Cultural",
+  "Indicador \n Capital \n Econômico",
+  "Escolaridade \n Pai",
+  "Escolaridade \n Mãe",IDHM) %>% na.exclude() %>% as.matrix()}
+correlation<-cor(my_matrix[[3]])
+
+png(file="corr.png", res=300, width=4500, height=4500)
+corrplot.mixed(correlation,
+         order="hclust",
+         upper = "ellipse",
+         lower.col = "black",number.cex=0.7,tl.cex=0.8,tl.col="black",
+         outline=T)
+dev.off()
 
 # Regressões -------------------------------------------------------------------
 
